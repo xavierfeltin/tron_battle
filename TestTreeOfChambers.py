82f2ded25bebd7ca14bf0c9ba7e1ec3015f09932
Xavier Folch <xavier.feltin@gmail.com>
2017-11-09 23:08:42 +0100

    articulation point done. Need to integrate it with tree of chambers

--- TestTreeOfChambers.py
+++ TestTreeOfChambers.py
@@ -42,7 +42,7 @@ class TestTreeOfChambers(unittest.TestCase):
         points = detect_articulation_points(area, (0, 0))
         self.assertEqual(len(points), 0)
 
-    def test_chamber_hall_list_articulation_points(self):
+    def test_chamber_list_articulation_points(self):
         '''
         3 articulations points expected when a chamber is on the map
         '''
@@ -56,6 +56,61 @@ class TestTreeOfChambers(unittest.TestCase):
         self.assertEqual(len(points), 3)
 
         expected_articulations = [(8, 3), (9, 3), (10, 3)]
+        for articulation in points:
+            self.assertTrue(articulation in expected_articulations)
+
+    def test_chamber_hall_list_articulation_points(self):
+        '''
+        3 articulations points expected when a chamber and a hall is on the map
+        '''
+        area = zeros((Configuration.MAX_X_GRID + 1, Configuration.MAX_Y_GRID + 1), dtype=bool)
+        for i in range(1, Configuration.MAX_Y_GRID+1): area[3, i] = 1
+        for i in range(3, 10):
+            area[i, Configuration.MAX_Y_GRID] = 1
+            area[i, 1] = 1
+        for i in range(1, 3): area[9, i] = 1
+        for i in range(4, Configuration.MAX_Y_GRID+1): area[9, i] = 1
+
+        points = detect_articulation_points(area, (9, 2))
+        self.assertEqual(len(points), 12)
+
+        expected_articulations = [(2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (8, 3), (9, 3), (10, 3)]
+        for articulation in points:
+            self.assertTrue(articulation in expected_articulations)
+
+    def test_complex_list_articulation_points(self):
+        '''
+        from: https://project.dke.maastrichtuniversity.nl/games/files/bsc/Kang_Bsc-paper.pdf(Fig 6.)
+        X articulations points expected when a chamber and a hall is on the map
+        '''
+        area = zeros((Configuration.MAX_X_GRID + 1, Configuration.MAX_Y_GRID + 1), dtype=bool)
+        wall_spaces = [(1, 1),(2, 1),(6, 1),(8, 1),(10, 1),(11, 1)
+            ,(1, 2),(6, 2),(8, 2),(11, 2)
+            ,(8, 3)
+            ,(5, 4),(6, 4),(7, 4),(8, 4),(9, 4),(10, 4)
+            ,(1, 5),(2, 5),(3, 5),(4, 5),(9, 5),(10,5)
+            ,(1, 6),(2, 6),(3, 6),(4, 6),(9, 6),(10,6),(11,6)
+            ,(3, 7),(9, 7),(10, 7),(11, 7)
+            ,(3, 8),(5, 8),(6, 8),(7, 8)
+            ,(3, 9),(4, 9),(5, 9)
+            ,(1, 10),(5, 10),(6, 10),(11, 10)
+            ,(1, 11),(2, 11),(6, 11),(10, 11),(11, 11)]
+
+        for wall in wall_spaces:
+            area[wall] = 1
+
+        for i in range(0, Configuration.MAX_X_GRID+1):
+            for j in range(13, Configuration.MAX_Y_GRID+1):
+                area[i,j] = 1
+
+        for i in range(13, Configuration.MAX_X_GRID+1):
+            for j in range(0, 13):
+                area[i,j] = 1
+
+        points = detect_articulation_points(area, (0, 0))
+        self.assertEqual(len(points), 12)
+
+        expected_articulations = [(2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (8, 3), (9, 3), (10, 3)]
         for articulation in points:
             self.assertTrue(articulation in expected_articulations)
 
